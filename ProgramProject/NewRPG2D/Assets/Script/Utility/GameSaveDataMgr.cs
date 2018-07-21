47d1a7c923860f95066ec17562667959975e3668
huffy <tom-hu@hotmail.com>
2018-07-21 11:49:29 +0800

    存取文件类

---
+++ ProgramProject/NewRPG2D/Assets/Script/Utility/GameSaveDataMgr.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Script.Utility
+{
+    public class GameSaveDataMgr : TSingleton<GameSaveDataMgr>
+    {
+
+        private string GetFilePath(string name)
+        {
+            string path = "";
+#if UNITY_EDITOR
+            path = string.Format("{0}/Game/", Application.persistentDataPath, name);
+
+#elif UNITY_IPHONE
+	   path = string.Format("{0}/Game/{1}", Application.persistentDataPath , name); 
+ 
+#elif UNITY_ANDROID
+	    path = string.Format("{0}/Game/{1}",  Application.persistentDataPath, name);
+#else
+        path = string.Format("{0}/Game/{1}",  Application.persistentDataPath, name);
+#endif
+            return path;
+        }
+
+        public void SaveGameData(string content, string fileName)
+        {
+            File.WriteAllText(fileName, content);
+        }
+
+        public string ReadGameData(string fileName)
+        {
+            string mPath = GetFilePath(fileName);
+            string content = "";
+            if (!File.Exists(mPath))
+            {
+                File.Create(mPath);
+            }
+            else
+            {
+                content = File.ReadAllText(mPath);
+            }
+            return content;
+        }
+    }
+}
