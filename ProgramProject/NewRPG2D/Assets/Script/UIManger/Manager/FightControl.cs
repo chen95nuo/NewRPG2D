9839e3253a76392e4b5765c789b1b18df5b53c1b
BaymaxGG <wys1078841314@vip.qq.com>
2018-08-28 20:10:30 +0800

    UI图片

---
+++ ProgramProject/NewRPG2D/Assets/Script/UIManger/Manager/FightControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightControl : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        GoFightMgr.CreateInstance();
+        DontDestroyOnLoad(this.gameObject);
+        if (GoFightMgr.instance.isRound)
+        {
+            TinyTeam.UI.TTUIPage.ShowPage<UIRoundPage>();
+            GoFightMgr.instance.isRound = false;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
