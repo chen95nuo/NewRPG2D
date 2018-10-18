47d336d7975c1c2a9a942ed36c19630495ab7c8a
BaymaxGG <wys1078841314@vip.qq.com>
2018-10-18 20:46:35 +0800

    UI动画更新

--- ProgramProject/NewRPG2D/Assets/Script/BuildingManger/Room/BuildBabyRoom.cs
+++ ProgramProject/NewRPG2D/Assets/Script/BuildingManger/Room/BuildBabyRoom.cs
@@ -6,10 +6,12 @@ public class BuildBabyRoom : RoomMgr
 {
     public override void AddRole(HallRole role)
     {
+        int index = 0;
         for (int i = 0; i < currentBuildData.roleData.Length; i++)
         {
             if (currentBuildData.roleData[i] == null)
             {
+                index++;
                 break;
             }
         }
