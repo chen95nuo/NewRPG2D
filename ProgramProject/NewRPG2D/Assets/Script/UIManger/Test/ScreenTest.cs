062f494fbbe072e55e03e50a9a939dd7eacfd146
BaymaxGG <wys1078841314@vip.qq.com>
2018-11-03 06:04:22 +0800

    Update

---
+++ ProgramProject/NewRPG2D/Assets/Script/UIManger/Test/ScreenTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTest : MonoBehaviour
+{
+
+    public int baseWidth = 1920;
+    public int baseHeight = 1080;
+    public Transform ts;
+
+    private void Awake()
+    {
+        ts = transform;
+    }
+    private void Start()
+    {
+        float size = Camera.main.orthographicSize;
+        float scale_x = Screen.width * 1f / baseWidth * 1f;
+        float scale_y = Screen.height * 1f / baseHeight * 1f;
+        ts.localScale = new Vector3(scale_x, scale_y, 1);
+        //Camera.main.orthographicSize = size * scale_y;
+    }
+}
