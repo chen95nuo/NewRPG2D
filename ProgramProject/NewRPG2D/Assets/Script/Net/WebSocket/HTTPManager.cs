6a86c90e0c2b29c9f0bbedd64246308912222db2
huffy <tom-hu@hotmail.com>
2018-11-10 17:01:53 +0800

    http

--- ProgramProject/NewRPG2D/Assets/Script/Net/WebSocket/HTTPManager.cs
+++ ProgramProject/NewRPG2D/Assets/Script/Net/WebSocket/HTTPManager.cs
@@ -6,19 +6,36 @@ using UnityEngine;
 
 namespace Assets.Script.Net
 {
+    public enum RequestTypeEnum
+    {
+        Get,
+        Post,
+    }
+
     public class HTTPManager : TSingleton<HTTPManager>
     {
-        private const string defaultUrl = "http://39.104.79.77:8882/user/regist?u={0}&p={1}";
+        private const string defaultUrl = "http://39.104.79.77:8882/user/regist";
+        private const string getUrl = "?u={0}&p={1}";
 
         public override void Init()
         {
             base.Init();
         }
 
-        public void Regist(string userName, string password)
+        public void Register(string userName, string password, RequestTypeEnum requestType = RequestTypeEnum.Get)
         {
-            HTTPRequest request = new HTTPRequest(new Uri(string.Format(defaultUrl, userName, password)), onRequestFinished);
-            request.Send();
+            if (requestType == RequestTypeEnum.Get)
+            {
+                HTTPRequest request = new HTTPRequest(new Uri(string.Format(defaultUrl + getUrl, userName, password)), onRequestFinished);
+                request.Send();
+            }
+            else if (requestType == RequestTypeEnum.Post)
+            {
+                HTTPRequest request = new HTTPRequest(new Uri(defaultUrl + "/posturi"), HTTPMethods.Post, onRequestFinished);
+                request.AddField("", userName);
+                request.AddField("", password);
+                request.Send();
+            }
         }
 
         private void onRequestFinished(HTTPRequest originalRequest, HTTPResponse response)
