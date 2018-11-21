a70e402f50b21e823bb40c1ef0076f8bcab3dcf2
huffy <tom-hu@hotmail.com>
2018-11-21 22:03:46 +0800

    头像修改

--- ProgramProject/NewRPG2D/Assets/Script/CaptureScreen.cs
+++ ProgramProject/NewRPG2D/Assets/Script/CaptureScreen.cs
@@ -12,6 +12,7 @@ public class CaptureScreen : MonoBehaviour
     [SerializeField] private Camera mCamera;
 
     private EquipmentRealProperty equipment;
+    private Dictionary<int, Sprite> roleIcon = new Dictionary<int, Sprite>();
 
     /// <summary>
     /// 获取人物头像
@@ -21,48 +22,60 @@ public class CaptureScreen : MonoBehaviour
     /// <param name="equipId">装备id</param>
     /// <param name="bForce">是否强制更新头像</param>
     /// <returns></returns>
-    public Texture2D CaptureScreenToTexture2D(int roleId, SexTypeEnum sexType, int equipId, bool bForce = false)
-    {
-        Texture2D texture = null;
-        if (CaptureScreenMgr.instance.IconDic.TryGetValue(roleId, out texture) == false || bForce)
-        {
-            manEquip.gameObject.SetActive(sexType == SexTypeEnum.Man);
-            womanEquip.gameObject.SetActive(sexType == SexTypeEnum.Woman);
-            equipment = EquipmentMgr.instance.GetEquipmentByEquipId(equipId);
-
-            if (sexType == SexTypeEnum.Man)
-            {
-                if (equipment == null)
-                {
-                    manEquip.ChangeOriginalEquip(EquipTypeEnum.Armor);
-                }
-                else
-                {
-                    manEquip.ChangeEquip(equipment.EquipType, equipment.EquipName);
-                }
-            }
-            else
-            {
-                if (equipment == null)
-                {
-                    womanEquip.ChangeOriginalEquip(EquipTypeEnum.Armor);
-                }
-                else
-                {
-                    womanEquip.ChangeEquip(equipment.EquipType, equipment.EquipName);
-                }
-            }
-            CaptureScreenMgr.instance.CaptureCamera(mCamera, new Rect(Vector2.zero, new Vector2(121, 140)), roleId);
-        }
-
-
-        return texture;
-    }
 
     public Sprite CaptureScreenToIcon(int roleId, SexTypeEnum sexType, int equipId, bool bForce = false)
     {
-        Texture2D texture = CaptureScreenToTexture2D(roleId, sexType, equipId, bForce);
+        Sprite icon = null;
+        if (bForce)
+        {
+            StartCoroutine(DelayCaptureIcon(roleId, sexType, equipId));
+            return icon;
+        }
+        else
+        {
+            if (roleIcon.TryGetValue(roleId, out icon) == false)
+            {
+                Texture2D texture = null;
+                if (CaptureScreenMgr.instance.IconDic.TryGetValue(roleId, out texture) == false)
+                {
+                    StartCoroutine(DelayCaptureIcon(roleId, sexType, equipId));
+                    return null;
+                }
+                icon = SpriteHelper.CreateSprite(texture);
+                roleIcon[roleId] = icon;
+            }
+            return icon;
+        }
+    }
 
-        return SpriteHelper.CreateSprite(texture);
+    public IEnumerator DelayCaptureIcon(int roleId, SexTypeEnum sexType, int equipId)
+    {
+        manEquip.gameObject.SetActive(sexType == SexTypeEnum.Man);
+        womanEquip.gameObject.SetActive(sexType == SexTypeEnum.Woman);
+        equipment = EquipmentMgr.instance.GetEquipmentByEquipId(equipId);
+        if (sexType == SexTypeEnum.Man)
+        {
+            if (equipment == null)
+            {
+                manEquip.ChangeOriginalEquip(EquipTypeEnum.Armor);
+            }
+            else
+            {
+                manEquip.ChangeEquip(equipment.EquipType, equipment.EquipName);
+            }
+        }
+        else
+        {
+            if (equipment == null)
+            {
+                womanEquip.ChangeOriginalEquip(EquipTypeEnum.Armor);
+            }
+            else
+            {
+                womanEquip.ChangeEquip(equipment.EquipType, equipment.EquipName);
+            }
+        }
+        yield return new WaitForSeconds(0.5f);
+        CaptureScreenMgr.instance.CaptureCamera(mCamera, new Rect(Vector2.zero, new Vector2(121, 140)), roleId);
     }
 }
